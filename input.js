c44e9405d657d258523248fae48678ed526549d3
Chelsea Dwarika <chelseakristina@live.com>
2022-10-28 22:27:17 +0000

    set up export of setupInput fn to play.js

--- input.js
+++ input.js
@@ -1,16 +1,15 @@
+const handleUserInput = i => {
+  if (i === '\u0003') { 
+    process.exit();
+  }
+};
+
 const setupInput = function () {
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding("utf8");
   stdin.resume();
-
-  const handleUserInput = i => {
-    if (i === '\u0003') { 
-      process.exit();
-    }
-};
   stdin.on("data", handleUserInput);
-
   return stdin;
 };
 
