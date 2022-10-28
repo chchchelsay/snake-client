20dfb67f29281b967518198ec01714c1c5548f50
Chelsea Dwarika <chelseakristina@live.com>
2022-10-28 22:23:26 +0000

    added ctrl+c exit command

--- play.js
+++ play.js
@@ -1,22 +1,22 @@
-const net = require("net");
-
-// establishes a connection with the game server
-const connect = function () {
-  const conn = net.createConnection({
-    host:"localhost", // IP address here,
-    port: 50541// PORT number here,
-  });
-
-  // interpret incoming data as text
-  conn.setEncoding("utf8");
-
-  conn.on("data", (data)=> {
-    console.log(data)
-});
-
-
-  return conn;
-};
+const {connect} = require("./client");
 
 console.log("Connecting ...");
 connect();
+
+// setup interface to handle user input from stdin
+const handleUserInput = i => {
+  if (i === '\u0003') { 
+    process.exit();
+  }
+};
+
+const setupInput = function () {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+  stdin.on("data", handleUserInput);
+  return stdin;
+};
+
+setupInput();
