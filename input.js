25d42e302053ab8754b3d392a88050e8e5b25b45
Chelsea Dwarika <chelseakristina@live.com>
2022-10-28 22:58:40 +0000

    WASD console.log additions & connection variable created

--- input.js
+++ input.js
@@ -1,10 +1,31 @@
+// Stores the active TCP connection object.
+let connection;
+
+/*handleUserInput:
+1) console.logs movement commands bound to keys WASD
+2) exits process when ctrl + c is pressed
+*/
+
 const handleUserInput = i => {
+  
+  if (i === "W" || i === "w") {
+    console.log("Move: up");
+  }
+  if (i === "A" || i === "a") {
+    console.log("Move: left");
+  }
+  if (i === "S" || i === "s") {
+    console.log("Move: down");
+  }
+  if (i === "D" || i === "d") {
+    console.log("Move: right");
+  }
   if (i === '\u0003') { 
     process.exit();
   }
 };
 
-const setupInput = function () {
+const setupInput = (conn) => {
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding("utf8");
@@ -12,6 +33,7 @@ const setupInput = function () {
   stdin.on("data", handleUserInput);
   return stdin;
 };
+
 
 module.exports = {
   setupInput
